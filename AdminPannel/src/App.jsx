89e04231ffda8c10463b3e25a83fba86a9a2e2b2
unknown <rakeshkumarrout001@gmail.com>
2026-07-31 20:54:09 +0530

    Done

--- AdminPannel/src/App.jsx
+++ AdminPannel/src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import VehicleManagement from "./Components/Profile/VehicleManagement/VehicleManagement";
+import FeatureListing from "./Components/Profile/FeatureListing/FeatureListing";
 
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/vehicle-management" element={<VehicleManagement />} />
+        <Route path="/feature-listing" element={<FeatureListing />} />
+
 
         
        
