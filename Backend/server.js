e8081504f30e30b0dc3a76dc75762aea738386f0
Akashroul <roulakashkumar2003@gmail.com>
2026-09-11 10:56:56 +0530

    pull

--- Backend/server.js
+++ Backend/server.js
@@ -98,18 +98,16 @@ app.use((req, res) => {
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/reviews_db")
-
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected Successfully");
+    console.log("Database:", mongoose.connection.name);
 
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
-
       console.log(`Locations API: http://localhost:${PORT}/api/locations`);
     });
   })
-
   .catch((err) => {
     console.error("Database connection error:", err);
   });
